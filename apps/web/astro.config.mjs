46dd88378e0e75ac4504b6c0a86a6610caeebd3b
Taqib <taqib.ibrahim@gmail.com>
2025-02-13 01:55:20 +0100

    chore(seo): add sitemap

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -7,9 +7,11 @@ import mdx from "@astrojs/mdx";
 
 import vercel from "@astrojs/vercel";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   site: "https://marblecms.com",
   adapter: vercel({
     webAnalytics: {
--- apps/web/src/pages/rss.xml.ts
+++ apps/web/src/pages/rss.xml.ts
@@ -4,28 +4,23 @@ import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
-  try {
-    const blog = await getCollection("blog");
+  const blog = await getCollection("blog");
 
-    const posts = [...blog].sort(
-      (a, b) =>
-        new Date(b.data.published).valueOf() -
-        new Date(a.data.published).valueOf(),
-    );
+  const posts = [...blog].sort(
+    (a, b) =>
+      new Date(b.data.published).valueOf() -
+      new Date(a.data.published).valueOf(),
+  );
 
-    return rss({
-      title: SITE.TITLE,
-      description: SITE.DESCRIPTION,
-      site: context.site ?? SITE.URL,
-      items: posts.map((post) => ({
-        title: post.data.title,
-        description: post.data.description,
-        pubDate: new Date(post.data.published),
-        link: `/${post.collection}/${post.id}/`,
-      })),
-    });
-  } catch (error) {
-    console.error("Error generating RSS feed:", error);
-    return new Response("Error generating RSS feed", { status: 500 });
-  }
+  return rss({
+    title: SITE.TITLE,
+    description: SITE.DESCRIPTION,
+    site: context.site ?? SITE.URL,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: new Date(post.data.published),
+      link: `/${post.collection}/${post.id}/`,
+    })),
+  });
 }
