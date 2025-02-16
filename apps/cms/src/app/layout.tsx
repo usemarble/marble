5c38c5e20f109c3947786a59c3c4459c2b58dae0
Taqib <taqib.ibrahim@gmail.com>
2025-02-16 01:40:45 +0100

    dashboard opengraph update

--- apps/cms/src/app/layout.tsx
+++ apps/cms/src/app/layout.tsx
@@ -6,14 +6,47 @@ import { Geist } from "next/font/google";
 import Providers from "./providers";
 
 export const metadata: Metadata = {
-  title: `${siteConfig.title}`,
+  title: siteConfig.title,
+  metadataBase: new URL(siteConfig.url),
   description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.title,
+    description: siteConfig.description,
+    images: [
+      {
+        url: `${siteConfig.url}/og.jpg`,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.title,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: `${siteConfig.url}/og.jpg`,
+        width: "1200",
+        height: "630",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/icon.svg",
+    apple: "/apple-icon.png",
+  },
 };
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+
 
 export default function RootLayout({
   children,
--- apps/cms/src/lib/site-config.ts
+++ apps/cms/src/lib/site-config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: "Marble",
-  description: "Super Simple CMS for Everyone",
-  url: "https://usetiles.com",
-  ogImage: "https://tiles.com/og.png",
+  description: "The smarter way to manage your blog",
+  url: "https://app.marblecms.com",
+  ogImage: "https://app.marblecms.com/og.jpg",
   keywords: ["CMS", "Content Management", "Headless CMS", "SEO", "Blogging"],
 };
