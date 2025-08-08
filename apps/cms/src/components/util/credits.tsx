a20e8e13b07ead6ed02764de102a991c28c92b37
Dominik K. <kochd.tin24@student.dhbw-heidenheim.de>
2025-08-08 15:40:40 +0200

    fix: no longer crash login (#54)

--- apps/cms/src/components/util/credits.tsx
+++ apps/cms/src/components/util/credits.tsx
@@ -12,17 +12,17 @@ const credits = {
     image: "https://unsplash.com/@lereverdo",
     text: "Valeria Reverdo",
   },
-} as const;
+};
 
 export default function Credits() {
   const { theme } = useTheme();
   return (
     <Link
-      href={credits[theme as keyof typeof credits].image}
+      href={credits[theme as keyof typeof credits ?? "dark"].image}
       target="_blank"
       className="text-sm font-medium text-muted-foreground hover:text-primary underline underline-offset-4"
     >
-      Image by {credits[theme as keyof typeof credits].text}
+      Image by {credits[theme as keyof typeof credits ?? "dark"]?.text}
     </Link>
   );
 }
