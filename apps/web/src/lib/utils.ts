38682264f10ac8da79f0fe318287f42c722d19fa
Taqib <taqib.ibrahim@gmail.com>
2025-10-26 13:25:23 +0100

    chore: merge tailwind classes

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,8 +1,15 @@
-export function calculateReadTime(content: string) {
-  const wordsPerMinute = 200;
-  const plainText = content.replace(/<[^>]*>/g, "").trim();
-  const wordCount = plainText.split(/\s+/).length;
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-  const readingTime = Math.ceil(wordCount / wordsPerMinute);
-  return readingTime;
+export function calculateReadTime(content: string) {
+	const wordsPerMinute = 200;
+	const plainText = content.replace(/<[^>]*>/g, "").trim();
+	const wordCount = plainText.split(/\s+/).length;
+
+	const readingTime = Math.ceil(wordCount / wordsPerMinute);
+	return readingTime;
+}
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
 }
