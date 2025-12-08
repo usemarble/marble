8f080a0411566e5828fb30bec4e2d78b242fbc46
Dominik K. <dominik@rivo.gg>
2025-12-08 02:25:45 +0100

    fix: switch to video on homepage

---
+++ packages/utils/src/constants/site.ts
@@ -0,0 +1,4 @@
+export const YOUTUBE_VIDEO_ID = "vAPVCCayBIA";
+
+export const YOUTUBE_EMBED_URL = `https://www.youtube.com/embed/${YOUTUBE_VIDEO_ID}`;
+
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,7 @@
 /** biome-ignore-all lint/performance/noBarrelFile: <> */
 export * from "./constants/api-key";
 export * from "./constants/pricing";
+export * from "./constants/site";
 export * from "./functions/api-key";
 export * from "./functions/highlight";
 export * from "./types/api-key";
