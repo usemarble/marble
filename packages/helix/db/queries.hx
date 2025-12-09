8bfa3b36e976ec07914d55ac025efd0495bdeda5
Dominik K. <dominik@rivo.gg>
2025-12-09 11:06:18 +0100

    chore: init helix

---
+++ packages/helix/db/queries.hx
@@ -0,0 +1,22 @@
+// Start writing your queries here.
+//
+// You can use the schema to help you write your queries.
+//
+// Queries take the form:
+//     QUERY {query name}({input name}: {input type}) =>
+//         {variable} <- {traversal}
+//         RETURN {variable}
+//
+// Example:
+//     QUERY GetUserFriends(user_id: String) =>
+//         friends <- N<User>(user_id)::Out<Knows>
+//         RETURN friends
+//
+//
+// For more information on how to write queries,
+// see the documentation at https://docs.helix-db.com
+// or checkout our GitHub at https://github.com/HelixDB/helix-db
+
+QUERY addUser(name: String) =>
+    user <- AddN<User>({name: name, age: 50})
+    RETURN user
