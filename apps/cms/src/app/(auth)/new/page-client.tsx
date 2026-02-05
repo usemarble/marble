33eee5d5904d33c1d90158ca7b8f67b9cd6a46c4
Taqib <taqib.ibrahim@gmail.com>
2026-02-05 02:39:56 +0100

    chore: format

--- apps/cms/src/app/(auth)/new/page-client.tsx
+++ apps/cms/src/app/(auth)/new/page-client.tsx
@@ -99,7 +99,9 @@ function PageClient() {
       <Card className="rounded-[20px] border-none bg-surface p-2 sm:w-[450px]">
         <div className="flex flex-col gap-6 rounded-[12px] bg-background p-6 shadow-xs">
           <CardHeader className="mb-0 items-center gap-0 text-center">
-            <CardTitle className="font-medium text-2xl">New workspace</CardTitle>
+            <CardTitle className="font-medium text-2xl">
+              New workspace
+            </CardTitle>
             <CardDescription className="text-center">
               {hasWorkspaces
                 ? "Set up your new workspace."
@@ -111,80 +113,80 @@ function PageClient() {
               className="flex flex-col gap-10"
               onSubmit={handleSubmit(onSubmit)}
             >
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col gap-2">
-                <Label className="sr-only" htmlFor="name">
-                  Name
-                </Label>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                  <Label className="sr-only" htmlFor="name">
+                    Name
+                  </Label>
 
-                <Input id="name" placeholder="Name" {...register("name")} />
-                {errors.name && (
-                  <ErrorMessage>{errors.name.message}</ErrorMessage>
-                )}
-              </div>
-              <div className="grid flex-1 gap-2">
-                <Label className="sr-only" htmlFor="slug">
-                  Slug
-                </Label>
-                <div className="flex w-full overflow-hidden rounded-md border border-input bg-transparent text-base shadow-xs transition-[color,box-shadow] placeholder:text-muted-foreground focus-within:border-ring focus-within:ring-[3px] focus-within:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:bg-input/30">
-                  <span className="border-r bg-muted p-2">
-                    {process.env.NEXT_PUBLIC_APP_URL?.split("//")[1]}/
-                  </span>
-
-                  <input
-                    id="slug"
-                    placeholder="Slug"
-                    {...register("slug")}
-                    autoComplete="off"
-                    className="w-full bg-transparent px-2 py-2 outline-none ring-0"
-                  />
+                  <Input id="name" placeholder="Name" {...register("name")} />
+                  {errors.name && (
+                    <ErrorMessage>{errors.name.message}</ErrorMessage>
+                  )}
                 </div>
-                {errors.slug && (
-                  <ErrorMessage>{errors.slug.message}</ErrorMessage>
-                )}
-              </div>
-              <div className="flex flex-col gap-2">
-                <Label className="sr-only" htmlFor="timezone">
-                  Timezone
-                </Label>
-                <Controller
-                  control={control}
-                  name="timezone"
-                  render={({ field }) => (
-                    <TimezoneSelector
-                      onValueChange={field.onChange}
-                      placeholder="Select timezone..."
-                      timezones={timezones}
-                      value={field.value}
+                <div className="grid flex-1 gap-2">
+                  <Label className="sr-only" htmlFor="slug">
+                    Slug
+                  </Label>
+                  <div className="flex w-full overflow-hidden rounded-md border border-input bg-transparent text-base shadow-xs transition-[color,box-shadow] placeholder:text-muted-foreground focus-within:border-ring focus-within:ring-[3px] focus-within:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:bg-input/30">
+                    <span className="border-r bg-muted p-2">
+                      {process.env.NEXT_PUBLIC_APP_URL?.split("//")[1]}/
+                    </span>
+
+                    <input
+                      id="slug"
+                      placeholder="Slug"
+                      {...register("slug")}
+                      autoComplete="off"
+                      className="w-full bg-transparent px-2 py-2 outline-none ring-0"
                     />
+                  </div>
+                  {errors.slug && (
+                    <ErrorMessage>{errors.slug.message}</ErrorMessage>
                   )}
-                />
-                {errors.timezone && (
-                  <ErrorMessage>{errors.timezone.message}</ErrorMessage>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label className="sr-only" htmlFor="timezone">
+                    Timezone
+                  </Label>
+                  <Controller
+                    control={control}
+                    name="timezone"
+                    render={({ field }) => (
+                      <TimezoneSelector
+                        onValueChange={field.onChange}
+                        placeholder="Select timezone..."
+                        timezones={timezones}
+                        value={field.value}
+                      />
+                    )}
+                  />
+                  {errors.timezone && (
+                    <ErrorMessage>{errors.timezone.message}</ErrorMessage>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <AsyncButton
+                  className="flex w-full cursor-pointer gap-2"
+                  isLoading={isSubmitting}
+                  type="submit"
+                >
+                  Create
+                </AsyncButton>
+                {hasWorkspaces && (
+                  <Link
+                    className={cn(
+                      buttonVariants({ variant: "ghost" }),
+                      "w-full"
+                    )}
+                    href="/"
+                  >
+                    Back to dashboard
+                  </Link>
                 )}
               </div>
-            </div>
-            <div className="flex flex-col gap-4">
-              <AsyncButton
-                className="flex w-full cursor-pointer gap-2"
-                isLoading={isSubmitting}
-                type="submit"
-              >
-                Create
-              </AsyncButton>
-              {hasWorkspaces && (
-                <Link
-                  className={cn(
-                    buttonVariants({ variant: "ghost", }),
-                    "w-full"
-                  )}
-                  href="/"
-                >
-                  Back to dashboard
-                </Link>
-              )}
-            </div>
-          </form>
+            </form>
           </CardContent>
         </div>
       </Card>
