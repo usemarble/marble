0aac3b2d1e355ea877f7d697619db519fb42d681
Taqib <taqib.ibrahim@gmail.com>
2026-08-17 14:11:02 +0100

    fix: allow new image hosts

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -15,7 +15,11 @@ export default defineConfig({
   site: "https://marblecms.com",
   trailingSlash: "never",
   image: {
-    domains: ["images.marblecms.com", "media.marblecms.com"],
+    domains: [
+      "images.marblecms.com",
+      "media.marblecms.com",
+      "cdn.marblecms.com",
+    ],
   },
   adapter: vercel({
     webAnalytics: {
